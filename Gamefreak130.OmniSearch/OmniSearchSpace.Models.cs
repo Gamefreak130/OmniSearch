2775f57df1fc2c2ff4b9ac728236c5a1b8ccb694
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-11-29 00:54:32 -0600

    Start search model framework

---
+++ Gamefreak130.OmniSearch/OmniSearchSpace.Models.cs
@@ -0,0 +1,73 @@
+﻿using Gamefreak130.OmniSearchSpace.Helpers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Gamefreak130.OmniSearchSpace.Helpers
+{
+    public class Document<T>
+    {
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public T Tag { get; }
+
+        public Document(string title, string description, T tag)
+        {
+            Title = title;
+            Description = description;
+            Tag = tag;
+        }
+    }
+}
+
+namespace Gamefreak130.OmniSearchSpace.Models
+{
+    public interface ISearchModel<T>
+    {
+        IEnumerable<T> Search(IEnumerable<Document<T>> documents, string query);
+    }
+
+    public abstract class SearchModel<T> : ISearchModel<T>
+    {
+        public abstract IEnumerable<T> Search(IEnumerable<Document<T>> documents, string query);
+
+        protected T LogWeight(Document<T> document, float weight)
+        {
+            Debugger.Log(0, "", $"{document.Title}: {weight}\n");
+            return document.Tag;
+        }
+    }
+
+    public class ExactMatch<T> : SearchModel<T>
+    {
+        public override IEnumerable<T> Search(IEnumerable<Document<T>> documents, string query)
+        {
+            query = query.ToLower();
+            return from document in documents
+                   // Little trick I learned from StackOverflow to efficiently count substring occurrences
+                   let titleWeight = (document.Title.Length - document.Title.ToLower().Replace(query, "").Length) / query.Length * PersistedSettings.kTitleWeight
+                   let descWeight = (document.Description.Length - document.Description.ToLower().Replace(query, "").Length) / query.Length * PersistedSettings.kDescriptionWeight
+                   let weight = titleWeight + descWeight
+                   where weight > 0
+                   orderby weight descending
+                   select LogWeight(document, weight);
+        }
+    }
+
+    /*public class TermFrequency : SearchModel
+    {
+        
+    }
+
+    public class TFIDFUnigram : SearchModel
+    {
+
+    }
+
+    public class TFIDFBigram : SearchModel
+    {
+
+    }*/
+}
---
+++ Gamefreak130.OmniSearch/OmniSearchSpace.PersistedSettings.cs
@@ -0,0 +1,13 @@
+﻿using Sims3.SimIFace;
+
+namespace Gamefreak130.OmniSearchSpace
+{
+    public static class PersistedSettings
+    {
+        [Tunable, TunableComment("Weight applied to the title/name of an object when searching")]
+        public static float kTitleWeight = 1f;
+
+        [Tunable, TunableComment("Weight applied to the description of an object when searching")]
+        public static float kDescriptionWeight = 0.5f;
+    }
+}
