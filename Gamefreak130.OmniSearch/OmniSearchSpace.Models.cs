598ca3a9b2ac9dee79e4b8a999ff6d79d3011dff
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-11-30 13:37:50 -0600

    Refactor TFIDF model a bit and add comments

--- Gamefreak130.OmniSearch/OmniSearchSpace.Models.cs
+++ Gamefreak130.OmniSearch/OmniSearchSpace.Models.cs
@@ -82,14 +82,21 @@ namespace Gamefreak130.OmniSearchSpace.Models
 
     public class TFIDFUnigram<T> : SearchModel<T>
     {
+        // CONSIDER maybe use a common abstract TFIDF class
         public override IEnumerable<T> Search(IEnumerable<Document<T>> documents, string query)
         {
-            // TODO Better documentation
             query = Regex.Replace(query.ToLower(), CHARS_TO_REMOVE, "");
             List<Document<T>> docList = documents.ToList();
-            List<Dictionary<string, double>> tfidfMatrix = new(docList.Count);
-            Dictionary<string, Dictionary<int, object>> wordOccurences = new();
 
+            // TFIDFMatrix is a set of document vector embeddings formed from the term frequency of words in each document weighted using TF-IDF
+            // The embedding itself is represented as a word dictionary, since the full vector of all words in the corpus would be extremely sparse
+            List<Dictionary<string, double>> tfidfMatrix = new(docList.Count);
+
+            // wordOccurences is a mapping of words to the documents containing them, allowing us to easily calculate IDF
+            // Using a HashSet of document indices rather than a document frequency count makes it easier to avoid double counting words appearing multiple times in a single document
+            Dictionary<string, HashSet<int>> wordOccurences = new();
+
+            // Iterate over every word of every document to calculate term and document frequency
             for (int i = 0; i < docList.Count; i++)
             {
                 Document<T> document = docList[i];
@@ -101,11 +108,12 @@ namespace Gamefreak130.OmniSearchSpace.Models
                         embedding[word] = default;
                     }
                     embedding[word] += PersistedSettings.kTitleWeight;
+
                     if (!wordOccurences.ContainsKey(word))
                     {
                         wordOccurences[word] = new();
                     }
-                    wordOccurences[word][i] = null;
+                    wordOccurences[word].Add(i);
                 }
                 foreach (string word in Regex.Split(Regex.Replace(document.Description, CHARS_TO_REMOVE, ""), TOKEN_SPLITTER))
                 {
@@ -114,22 +122,26 @@ namespace Gamefreak130.OmniSearchSpace.Models
                         embedding[word] = default;
                     }
                     embedding[word] += PersistedSettings.kDescriptionWeight;
+
                     if (!wordOccurences.ContainsKey(word))
                     {
                         wordOccurences[word] = new();
                     }
-                    wordOccurences[word][i] = null;
+                    wordOccurences[word].Add(i);
                 }
                 tfidfMatrix.Add(embedding);
             }
+
+            // Iterate over every document again to turn TF vector embeddings into TF-IDF embeddings
             for (int i = 0; i < docList.Count; i++)
             {
-                foreach (var group in new Dictionary<string, double>(tfidfMatrix[i]).Select(kvp => new { word = kvp.Key, count = kvp.Value }))
+                foreach (KeyValuePair<string, double> kvp in new List<KeyValuePair<string, double>>(tfidfMatrix[i]))
                 {
-                    tfidfMatrix[i][group.word] = Math.Log10(1 + group.count) * Math.Log10(docList.Count / wordOccurences[group.word].Count);
+                    tfidfMatrix[i][kvp.Key] = Math.Log10(1 + kvp.Value) * Math.Log10(docList.Count / wordOccurences[kvp.Key].Count);
                 }
             }
 
+            // Calculate TF-IDF vector embedding for the query, as well as its magnitude
             Dictionary<string, double> queryVector = new();
             double queryMagnitude = 0;
             foreach (var group in Regex.Split(query, TOKEN_SPLITTER).GroupBy(word => word, (word, elements) => new { word, count = elements.Count() }))
@@ -143,32 +155,37 @@ namespace Gamefreak130.OmniSearchSpace.Models
             }
             queryMagnitude = Math.Sqrt(queryMagnitude);
 
-            if (queryMagnitude > 0)
+            // If the magnitude of the query vector is 0, then the query has no words in common with any documents in the corpus
+            // Thus, we return an empty set of results
+            if (queryMagnitude <= 0)
             {
-                List<double> similarities = new(docList.Count);
-                for (int i = 0; i < docList.Count; i++)
-                {
-                    Dictionary<string, double> documentVector = tfidfMatrix[i];
-                    double docMagnitude = 0;
-                    double dot = 0;
-                    foreach (string word in documentVector.Keys)
-                    {
-                        double tfidf = documentVector[word];
-                        if (queryVector.ContainsKey(word))
-                        {
-                            dot += tfidf * queryVector[word];
-                        }
-                        docMagnitude += tfidf * tfidf;
-                    }
-                    docMagnitude = Math.Sqrt(docMagnitude);
-                    similarities.Add(docMagnitude != 0 ? dot / (docMagnitude * queryMagnitude) : 0);
-                }
-                return docList.Select((doc, i) => new { doc, i })
-                              .Where(x => similarities[x.i] != 0)
-                              .OrderByDescending(x => similarities[x.i])
-                              .Select(x => LogWeight(x.doc, (float)similarities[x.i]));
+                return new T[0];
             }
-            return new T[0];
+
+            // Calculate cosine similarity (dot product divided by product of magnitudes) between the query vector and each document vector
+            List<double> similarities = new(docList.Count);
+            for (int i = 0; i < docList.Count; i++)
+            {
+                Dictionary<string, double> documentVector = tfidfMatrix[i];
+                double docMagnitude = 0;
+                double dot = 0;
+                foreach (string word in documentVector.Keys)
+                {
+                    double tfidf = documentVector[word];
+                    if (queryVector.ContainsKey(word))
+                    {
+                        dot += tfidf * queryVector[word];
+                    }
+                    docMagnitude += tfidf * tfidf;
+                }
+                docMagnitude = Math.Sqrt(docMagnitude);
+                similarities.Add(docMagnitude != 0 ? dot / (docMagnitude * queryMagnitude) : 0);
+            }
+
+            return docList.Select((doc, i) => new { doc, i })
+                          .Where(x => similarities[x.i] != 0)
+                          .OrderByDescending(x => similarities[x.i])
+                          .Select(x => LogWeight(x.doc, (float)similarities[x.i]));
         }
     }
 
