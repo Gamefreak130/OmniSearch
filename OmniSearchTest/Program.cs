5751278e40e43a3bbf3d4df81ed4f84ce9fb0cb8
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-12-01 19:38:04 -0600

    Cleanup test queries, make test method generic for all search models, and time process duration

--- OmniSearchTest/Program.cs
+++ OmniSearchTest/Program.cs
@@ -1,56 +1,69 @@
 ﻿using Gamefreak130.OmniSearchSpace.Helpers;
 using Gamefreak130.OmniSearchSpace.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Xml;
 
-namespace OmniSearchTest
-{
-    class Program
-    {
-        static void Main(string[] _)
-        {
-            var exactMatch = new ExactMatch<object>();
-            var tf = new TermFrequency<object>();
-            var tfidf = new TFIDFUnigram<object>();
-            RunQuery(@"Furniture Scrape\Buy\Appliances__Export_ABC106B2.xml", "kno", exactMatch);
-            RunQuery(@"Furniture Scrape\Buy\Appliances__Export_ABC106B2.xml", "kno", tf);
-            RunQuery(@"Furniture Scrape\Build\Trees__Export_3CF703D8.xml", "small tree", exactMatch);
-            RunQuery(@"Furniture Scrape\Build\Trees__Export_3CF703D8.xml", "small      \ntree", tf);
-            RunQuery(@"Furniture Scrape\Build\Trees__Export_3CF703D8.xml", "small      \ntree", tfidf);
-            RunQuery(@"Furniture Scrape\Buy\Entertainment__Export_702F7408.xml", "gnubb", tfidf);
-            RunQuery(@"Furniture Scrape\Build\WallPaint__Export_5E06D03C.xml", "baseboard", tfidf);
-            RunQuery(@"Furniture Scrape\Buy\Comfort__Export_1A1B0653.xml", "modern chair", tfidf);
-            RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "rustic painting", tf);
-            RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "rustic painting", tfidf);
-            RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "small wall with incredibly thick paint and a shining rock face on the cliff facing the sky and smiling", tfidf);
-            RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "arcturus tiktok", tfidf);
-            Debugger.Break();
-        }
+#if DEBUG
+Debugger.Break();
+#endif
+Helpers.RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "gnom");
+Helpers.RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "gnome");
+Helpers.RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "modern sculpture");
+Helpers.RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "into the future sculpture");
+Helpers.RunQuery(@"Furniture Scrape\Buy\Decor__Export_A2ED8725.xml", "small wall with incredibly thick paint and a shining rock face on the cliff facing the sky and smiling");
+#if !DEBUG
+Debugger.Break();
+#endif
 
-        static void RunQuery(string inFile, string query, ISearchModel<object> model)
+static class Helpers
+{
+    public static void RunQuery(string inFile, string query)
+    {
+        // Get all available search models
+        List<ISearchModel<object>> models = new List<Type>(typeof(ISearchModel<>).Assembly.GetTypes()).FindAll(type => !type.IsAbstract && !type.IsNested && type.Namespace == "Gamefreak130.OmniSearchSpace.Models")
+                                                                                                      .ConvertAll(type => type.MakeGenericType(typeof(object))
+                                                                                                                              .GetConstructor(new Type[0])
+                                                                                                                              .Invoke(null) as ISearchModel<object>);
+        // Load documents from corpus XML
+        List<Document<object>> docs = new();
+        using (XmlReader reader = XmlReader.Create(inFile))
         {
-            List<Document<object>> docs = new();
-            using (XmlReader reader = XmlReader.Create(inFile))
+            for (bool notEnd = reader.ReadToFollowing("Item"); notEnd; notEnd = reader.ReadToNextSibling("Item"))
             {
-                for (bool notEnd = reader.ReadToFollowing("Item"); notEnd; notEnd = reader.ReadToNextSibling("Item"))
+                using (XmlReader curNode = reader.ReadSubtree())
                 {
-                    using (XmlReader curNode = reader.ReadSubtree())
-                    {
-                        curNode.ReadToDescendant("Title");
-                        string title = XmlConvert.DecodeName(curNode.ReadElementContentAsString());
-                        string description = curNode.ReadToNextSibling("Description") ? XmlConvert.DecodeName(curNode.ReadElementContentAsString()) : "";
-                        docs.Add(new(title, description, null));
-                    }
+                    curNode.ReadToDescendant("Title");
+                    string title = XmlConvert.DecodeName(curNode.ReadElementContentAsString());
+                    string description = curNode.ReadToNextSibling("Description") ? XmlConvert.DecodeName(curNode.ReadElementContentAsString()) : "";
+                    docs.Add(new(title, description, null));
                 }
             }
+        }
 
-            Debugger.Log(0, "", $"RESULTS FOR \"{query}\":\n\n");
-            if (new List<object>(model.Search(docs, query)).Count == 0)
+        // Conduct search using each available model
+        foreach (var model in models)
+        {
+            Type modelType = model.GetType();
+            Debugger.Log(0, "", $"RESULTS FOR \"{query}\" using {modelType.Name.Substring(0, modelType.Name.IndexOf("`"))}:\n\n");
+
+            DateTime start = DateTime.Now;
+            int count = 0;
+
+            // Although we're merely incrementing a count here, the procedure to select the item will log info to the debugger,
+            // Simulating a significantly complex in-game procedure such as loading item thumbnails and generating UI elements
+            foreach (object _ in model.Search(docs, query))
             {
-                Debugger.Log(0, "", "N/A");
+                count++;
             }
-            Debugger.Log(0, "", "\n\n");
+
+            Debugger.Log(0, "", $"{count} results\n\n");
+
+            Debugger.Log(0, "", $"Took about {(DateTime.Now - start).TotalMilliseconds} milliseconds\n\n");
+#if DEBUG
+            Debugger.Break();
+#endif
         }
     }
 }
