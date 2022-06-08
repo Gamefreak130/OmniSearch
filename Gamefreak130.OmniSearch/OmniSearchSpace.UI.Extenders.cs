d4c1f63e7515e1933b8749241b5a7c4e40bfb7fa
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2022-06-08 11:25:33 -0500

    Clear search bar and search model on eyedropper

--- Gamefreak130.OmniSearch/OmniSearchSpace.UI.Extenders.cs
+++ Gamefreak130.OmniSearch/OmniSearchSpace.UI.Extenders.cs
@@ -674,7 +674,7 @@ namespace Gamefreak130.OmniSearchSpace.UI.Extenders
         }
     }
     // CONSIDER BuildCatalogItem vs. BuildPatternItem in search results
-    // TODO Clear and SetSearchModel when eyedropper from one catalog grid to another (or different tabs of a single middle puck) (or collections -> build objects)
+    // TODO Stop clearing search bar on eyedropper or pickup
     public class BuildExtender : BuildBuyExtender
     {
         protected override IEnumerable<Document<object>> Corpus => (BuildController.sController.mProductList ?? BuildController.sController.mPresetList).Select(SelectDocument);
@@ -684,40 +684,42 @@ namespace Gamefreak130.OmniSearchSpace.UI.Extenders
             SetSearchBarVisibility();
             SearchBar.MoveToBack();
 
+            World.OnEyedropperPickCallback += OnEyedropperPick;
+
             BuildController controller = BuildController.sController;
 
-            controller.mPoolSelectObjectsButton.Click += OnCategoryButtonClick;
-            controller.mPoolObjectsSelectPoolButton.Click += OnCategoryButtonClick;
+            controller.mPoolSelectObjectsButton.Click += OnCategorySelected;
+            controller.mPoolObjectsSelectPoolButton.Click += OnCategorySelected;
 
-            controller.mFountainSelectObjects.Click += OnCategoryButtonClick;
-            controller.mFountainObjectsSelectFountainButton.Click += OnCategoryButtonClick;
+            controller.mFountainSelectObjects.Click += OnCategorySelected;
+            controller.mFountainObjectsSelectFountainButton.Click += OnCategorySelected;
 
-            controller.mWallPaintSelectWallButton.Click += OnCategoryButtonClick;
-            controller.mWallCreateSelectPaintButton.Click += OnCategoryButtonClick;
-            controller.mWallCreateHalfWallButton.Click += OnCategoryButtonClick;
-            controller.mHalfWallCreateSelectPaintButton.Click += OnCategoryButtonClick;
-            controller.mHalfWallCreateWallButton.Click += OnCategoryButtonClick;
-            controller.mHalfWallCreateRoomButton.Click += OnCategoryButtonClick;
-            controller.mHalfWallCreateDiagonalRoomButton.Click += OnCategoryButtonClick;
+            controller.mWallPaintSelectWallButton.Click += OnCategorySelected;
+            controller.mWallCreateSelectPaintButton.Click += OnCategorySelected;
+            controller.mWallCreateHalfWallButton.Click += OnCategorySelected;
+            controller.mHalfWallCreateSelectPaintButton.Click += OnCategorySelected;
+            controller.mHalfWallCreateWallButton.Click += OnCategorySelected;
+            controller.mHalfWallCreateRoomButton.Click += OnCategorySelected;
+            controller.mHalfWallCreateDiagonalRoomButton.Click += OnCategorySelected;
 
-            controller.mFenceCreateGateButton.Click += OnCategoryButtonClick;
-            controller.mGatesCreateFenceButton.Click += OnCategoryButtonClick;
+            controller.mFenceCreateGateButton.Click += OnCategorySelected;
+            controller.mGatesCreateFenceButton.Click += OnCategorySelected;
 
-            controller.mStairsCatalogRailingsButton.Click += OnCategoryButtonClick;
-            controller.mStairsCatalogElevatorsButton.Click += OnCategoryButtonClick;
-            controller.mRailingsCatalogStairsButton.Click += OnCategoryButtonClick;
-            controller.mRailingsCatalogElevatorsButton.Click += OnCategoryButtonClick;
-            controller.mElevatorsCatalogStairsButton.Click += OnCategoryButtonClick;
-            controller.mElevatorsCatalogRailingsButton.Click += OnCategoryButtonClick;
+            controller.mStairsCatalogRailingsButton.Click += OnCategorySelected;
+            controller.mStairsCatalogElevatorsButton.Click += OnCategorySelected;
+            controller.mRailingsCatalogStairsButton.Click += OnCategorySelected;
+            controller.mRailingsCatalogElevatorsButton.Click += OnCategorySelected;
+            controller.mElevatorsCatalogStairsButton.Click += OnCategorySelected;
+            controller.mElevatorsCatalogRailingsButton.Click += OnCategorySelected;
 
-            controller.mFoundationCatalogSquareButton.Click += OnCategoryButtonClick;
-            controller.mFoundationCatalogDiagonalButton.Click += OnCategoryButtonClick;
+            controller.mFoundationCatalogSquareButton.Click += OnCategorySelected;
+            controller.mFoundationCatalogDiagonalButton.Click += OnCategorySelected;
 
-            (controller.GetChildByID((uint)BuildController.ControlID.TerrainPaintButton, true) as Button).Click += OnCategoryButtonClick;
-            (controller.GetChildByID((uint)BuildController.ControlID.TerrainEraseButton, true) as Button).Click += OnCategoryButtonClick;
+            (controller.GetChildByID((uint)BuildController.ControlID.TerrainPaintButton, true) as Button).Click += OnCategorySelected;
+            (controller.GetChildByID((uint)BuildController.ControlID.TerrainEraseButton, true) as Button).Click += OnCategorySelected;
 
-            BuildController.sController.mCollectionGrid.ItemClicked += (_, _) => SetSearchModel();
-            BuildController.sController.mCatalogProductFilter.FiltersChanged += SetSearchModel;
+            controller.mCollectionGrid.ItemClicked += (_, _) => SetSearchModel();
+            controller.mCatalogProductFilter.FiltersChanged += SetSearchModel;
 
             controller.mConstructionCatalogTabContainer.TabSelect += OnTabSelect;
             controller.mFloorPaintTabContainer.TabSelect += OnTabSelect;
@@ -732,11 +734,11 @@ namespace Gamefreak130.OmniSearchSpace.UI.Extenders
             controller.mCollectionCatalogWindow.VisibilityChange += OnMiddlePuckVisibilityChange;
         }
 
-        /*public override void Dispose()
+        public override void Dispose()
         {
-            World.OnEyedropperPickCallback
+            World.OnEyedropperPickCallback -= OnEyedropperPick;
             base.Dispose();
-        }*/
+        }
 
         protected override void ClearCatalogGrid()
         {
@@ -816,7 +818,7 @@ namespace Gamefreak130.OmniSearchSpace.UI.Extenders
             SearchBar.SetLocation(x, y, width);
         }
 
-        private void OnCategoryButtonClick(WindowBase _, UIButtonClickEventArgs __)
+        private void OnCategorySelected(object _, EventArgs __)
         {
             SearchBar.Clear();
             SetSearchBarVisibility();
@@ -825,6 +827,12 @@ namespace Gamefreak130.OmniSearchSpace.UI.Extenders
         private void OnTabSelect(TabControl _, TabControl __) => SetSearchModel();
 
         private void OnMiddlePuckVisibilityChange(WindowBase _, UIVisibilityChangeEventArgs __) => SetSearchBarVisibility();
+
+        private void OnEyedropperPick(object _, EventArgs __)
+        {
+            OnCategorySelected(_, __);
+            ProcessExistingQuery();
+        }
 
         protected override void QueryEnteredTask()
         {
